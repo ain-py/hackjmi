cba50a9063135668a45304e6d8066ea2c6e45c22
AIN <mdnoor20205@gmail.com>
2023-02-05 14:03:23 +0530

    added dynamic data in pie chart

--- lib/screens/add_category.dart
+++ lib/screens/add_category.dart
@@ -1,7 +1,10 @@
+import 'package:app/utils/colors.dart';
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/src/widgets/placeholder.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class AddCategory extends StatefulWidget {
   const AddCategory({super.key});
@@ -12,11 +15,36 @@ class AddCategory extends StatefulWidget {
 
 class _AddCategoryState extends State<AddCategory> {
   bool? _value = false;
-  String gender = "male";
+  String type = "expense";
+  String _selectedType = '';
+  Widget _customDropDownAddress(BuildContext context, String? item) {
+    return Text(
+      item ?? 'Choose a category',
+      style: Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 16),
+    );
+  }
+
+  Widget _style1(BuildContext context, String? item, bool isSelected) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text(
+            item ?? '',
+            style:
+                Theme.of(context).textTheme.titleLarge!.copyWith(fontSize: 16),
+          ),
+        ),
+        Divider(
+          thickness: 1,
+        ),
+      ],
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.grey[400],
+      backgroundColor: colors.gray,
       appBar: AppBar(
         title: Text('Add Category'),
       ),
@@ -43,7 +71,6 @@ class _AddCategoryState extends State<AddCategory> {
                 width: double.infinity,
                 decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(8),
-                  color: Colors.white,
                 ),
                 // padding: EdgeInsets.all(8),
                 // margin: EdgeInsets.all(8),
@@ -51,21 +78,21 @@ class _AddCategoryState extends State<AddCategory> {
                   children: [
                     RadioListTile(
                       title: Text("Expense"),
-                      value: "male",
-                      groupValue: gender,
+                      value: "expense",
+                      groupValue: type,
                       onChanged: (value) {
                         setState(() {
-                          gender = value.toString();
+                          type = value.toString();
                         });
                       },
                     ),
                     RadioListTile(
                       title: Text("Income"),
-                      value: "female",
-                      groupValue: gender,
+                      value: "income",
+                      groupValue: type,
                       onChanged: (value) {
                         setState(() {
-                          gender = value.toString();
+                          type = value.toString();
                         });
                       },
                     ),
@@ -75,22 +102,28 @@ class _AddCategoryState extends State<AddCategory> {
               SizedBox(
                 height: 5,
               ),
-              Text(
-                "Description",
-                style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold),
-              ),
+              NewWidget(),
               SizedBox(
                 height: 8,
               ),
-              TextField(
-                decoration: InputDecoration(
-                  border: OutlineInputBorder(),
-                  hintText: 'Enter a search term',
-                  filled: true,
-                  fillColor: Colors.white,
+              DropdownSearch<String>(
+                items: ['Food & Drinks', 'Shoping'],
+                onChanged: (value) {
+                  _selectedType = value!;
+                  print(_selectedType);
+                },
+                dropdownBuilder: _customDropDownAddress,
+                //   selectedItem: _selectedType,
+                popupProps: PopupProps.menu(
+                  itemBuilder: _style1,
+                  showSelectedItems: true,
+                ),
+                dropdownDecoratorProps: DropDownDecoratorProps(
+                  dropdownSearchDecoration: InputDecoration(
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(14)),
+                      fillColor: Colors.white,
+                      filled: true),
                 ),
               ),
               SizedBox(
@@ -110,7 +143,8 @@ class _AddCategoryState extends State<AddCategory> {
                 keyboardType: TextInputType.number,
                 inputFormatters: [FilteringTextInputFormatter.digitsOnly],
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14)),
                   hintText: '0.0',
                   filled: true,
                   fillColor: Colors.white,
@@ -131,7 +165,8 @@ class _AddCategoryState extends State<AddCategory> {
               ),
               TextField(
                 decoration: InputDecoration(
-                  border: OutlineInputBorder(),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(14)),
                   hintText: 'Note',
                   filled: true,
                   fillColor: Colors.white,
@@ -142,10 +177,10 @@ class _AddCategoryState extends State<AddCategory> {
               ),
               Center(
                 child: SizedBox(
-                  width: 200,
+                  width: 300,
                   height: 50,
                   child: ElevatedButton(
-                    child: Text('Submit',
+                    child: Text('Save',
                         style: TextStyle(
                             fontSize: 18, fontWeight: FontWeight.bold)),
                     onPressed: () {
@@ -158,6 +193,21 @@ class _AddCategoryState extends State<AddCategory> {
           ),
         ),
       ),
+    );
+  }
+}
+
+class NewWidget extends StatelessWidget {
+  const NewWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      "Category",
+      style: TextStyle(
+          color: Colors.black, fontSize: 18, fontWeight: FontWeight.bold),
     );
   }
 }
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,14 +1,15 @@
 import 'dart:convert';
 import 'dart:io';
-
 import 'package:app/models/expense_model.dart';
+import 'package:app/provider/main_provider.dart';
 import 'package:app/utils/constants.dart';
 import 'package:bottom_navy_bar/bottom_navy_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pie_chart/pie_chart.dart';
 import 'package:app/utils/colors.dart';
-import 'package:app/utils/constants.dart';
+import 'package:provider/provider.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -18,23 +19,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  Future<String> readJsonFile(String filePath) async {
-    return await File(filePath).readAsString();
-  }
-
-  void getExpense() async {
-    var expenseData =
-        expenseDataFromJson(await rootBundle.loadString('assets/test.json'));
-    print(expenseData.expense);
-    var newExp =
-        Expense(amount: 10, note: "hello", desc: "this is rhe", date: 123333);
-    expenseData.expense.add(newExp);
-    expenseData.expense.forEach((element) {
-      print(element.desc);
-    });
-    print(expenseDataToJson(expenseData));
-  }
-
   Map<String, double> dataMap = {
     "Flutter": 5,
     "React": 3,
@@ -49,9 +33,16 @@ class _HomePageState extends State<HomePage> {
     const Color(0xff6c5ce7),
   ];
   @override
+  void initState() {
+    // TODO: implement initState
+
+    Provider.of<MainProvider>(context, listen: false).getExpense();
+    super.initState();
+  }
+
+  @override
   int _currentIndex = 0;
   Widget build(BuildContext context) {
-    getExpense();
     return Scaffold(
       backgroundColor: colors.gray,
       appBar: AppBar(
@@ -89,161 +80,168 @@ class _HomePageState extends State<HomePage> {
           ),
         ],
       ),
-      body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
-              color: Colors.white,
-              boxShadow: kboxShadow,
-            ),
-            padding: EdgeInsets.all(8),
-            margin: EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Expenses Structure",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "Last 30 Days",
-                  style: Theme.of(context).textTheme.labelMedium,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  "₹ 10,000",
-                  style: Theme.of(context).textTheme.labelLarge,
-                ),
-                SizedBox(
-                  height: 3,
-                ),
-                PieChart(
-                  dataMap: dataMap,
-                  animationDuration: Duration(milliseconds: 800),
-                  chartLegendSpacing: 32,
-                  chartRadius: MediaQuery.of(context).size.width / 3.2,
-                  colorList: colorList,
-                  initialAngleInDegree: 0,
-                  chartType: ChartType.disc,
-                  ringStrokeWidth: 32,
-                  centerText: "HYBRID",
-                  legendOptions: LegendOptions(
-                    showLegendsInRow: false,
-                    legendPosition: LegendPosition.right,
-                    showLegends: true,
-                    legendShape: BoxShape.circle,
-                    legendTextStyle: TextStyle(
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  chartValuesOptions: ChartValuesOptions(
-                    showChartValueBackground: true,
-                    showChartValues: true,
-                    showChartValuesInPercentage: false,
-                    showChartValuesOutside: false,
-                    decimalPlaces: 1,
-                  ),
-                  // gradientList: ---To add gradient colors---
-                  // emptyColorGradient: ---Empty Color gradient---
-                ),
-              ],
-            ),
-          ),
-          Container(
-            width: double.infinity,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(8),
-                color: Colors.white,
-                boxShadow: kboxShadow),
-            padding: EdgeInsets.all(8),
-            margin: EdgeInsets.all(8),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  "Last records overview",
-                  style: Theme.of(context).textTheme.titleLarge,
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Text("Last 30 Days",
-                    style: Theme.of(context)
-                        .textTheme
-                        .labelMedium!
-                        .copyWith(color: Colors.black54)),
-                SizedBox(
-                  height: 5,
-                ),
-                //Item rows
-
-                ListTile(
-                  leading: CircleAvatar(
-                    backgroundColor: Colors.red[300],
-                    child: Icon(
-                      Icons.dangerous,
+      body: Consumer<MainProvider>(
+        builder: (context, provider, child) => provider.isLoadingHome
+            ? Center(child: CircularProgressIndicator())
+            : SingleChildScrollView(
+                child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(8),
                       color: Colors.white,
+                      boxShadow: kboxShadow,
+                    ),
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Expenses Structure",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "Last 30 Days",
+                          style: Theme.of(context).textTheme.labelMedium,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          "₹ ${provider.totalSpend.toStringAsFixed(2)}",
+                          style: Theme.of(context).textTheme.labelLarge,
+                        ),
+                        SizedBox(
+                          height: 3,
+                        ),
+                        PieChart(
+                          dataMap: provider.pieData,
+                          animationDuration: Duration(milliseconds: 800),
+                          chartLegendSpacing: 32,
+                          chartRadius: MediaQuery.of(context).size.width / 3.2,
+                          colorList: colorList,
+                          initialAngleInDegree: 0,
+                          chartType: ChartType.disc,
+                          ringStrokeWidth: 32,
+                          centerText: "Expense",
+                          legendOptions: LegendOptions(
+                            showLegendsInRow: false,
+                            legendPosition: LegendPosition.right,
+                            showLegends: true,
+                            legendShape: BoxShape.circle,
+                            legendTextStyle: TextStyle(
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          chartValuesOptions: ChartValuesOptions(
+                            showChartValueBackground: true,
+                            showChartValues: true,
+                            showChartValuesInPercentage: false,
+                            showChartValuesOutside: false,
+                            decimalPlaces: 1,
+                          ),
+                          // gradientList: ---To add gradient colors---
+                          // emptyColorGradient: ---Empty Color gradient---
+                        ),
+                      ],
                     ),
                   ),
-                  title: Text(
-                    'Food & Drinks',
-                    style: Theme.of(context)
-                        .textTheme
-                        .titleLarge!
-                        .copyWith(fontSize: 16),
-                  ),
-                  subtitle: Text('Today'),
-                  trailing: Column(
-                    children: [
-                      SizedBox(
-                        height: 10,
-                      ),
-                      Text("-₹5000",
-                          style: TextStyle(color: Colors.red, fontSize: 15)),
-                      SizedBox(
-                        height: 5,
-                      ),
-                      Text(
-                        'Yesterday',
-                        style: Theme.of(context)
-                            .textTheme
-                            .labelMedium!
-                            .copyWith(color: Colors.black54, fontSize: 14),
-                      )
-                    ],
-                  ),
-                ),
-                Container(
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Icon(Icons.phone),
-                      SizedBox(
-                        width: 20,
-                      ),
-                      Text("Others", style: TextStyle(fontSize: 15)),
-                      Spacer(),
-                      Text("-129",
-                          style: TextStyle(color: Colors.red, fontSize: 15)),
-                      SizedBox(
-                        width: 20,
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          )
-        ],
-      )),
+                  Container(
+                    width: double.infinity,
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8),
+                        color: Colors.white,
+                        boxShadow: kboxShadow),
+                    padding: EdgeInsets.all(8),
+                    margin: EdgeInsets.all(8),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          "Last records overview",
+                          style: Theme.of(context).textTheme.titleLarge,
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text("Last 30 Days",
+                            style: Theme.of(context)
+                                .textTheme
+                                .labelMedium!
+                                .copyWith(color: Colors.black54)),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        //Item rows
+
+                        ListTile(
+                          leading: CircleAvatar(
+                            backgroundColor: Colors.red[300],
+                            child: FaIcon(
+                              FontAwesomeIcons.bowlFood,
+                              color: Colors.white,
+                            ),
+                          ),
+                          title: Text(
+                            'Food & Drinks',
+                            style: Theme.of(context)
+                                .textTheme
+                                .titleLarge!
+                                .copyWith(fontSize: 16),
+                          ),
+                          subtitle: Text('Today'),
+                          trailing: Column(
+                            children: [
+                              SizedBox(
+                                height: 10,
+                              ),
+                              Text("-₹5000",
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 15)),
+                              SizedBox(
+                                height: 5,
+                              ),
+                              Text(
+                                'Yesterday',
+                                style: Theme.of(context)
+                                    .textTheme
+                                    .labelMedium!
+                                    .copyWith(
+                                        color: Colors.black54, fontSize: 14),
+                              )
+                            ],
+                          ),
+                        ),
+                        Container(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              Icon(Icons.phone),
+                              SizedBox(
+                                width: 20,
+                              ),
+                              Text("Others", style: TextStyle(fontSize: 15)),
+                              Spacer(),
+                              Text("-129",
+                                  style: TextStyle(
+                                      color: Colors.red, fontSize: 15)),
+                              SizedBox(
+                                width: 20,
+                              ),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              )),
+      ),
     );
   }
 }
